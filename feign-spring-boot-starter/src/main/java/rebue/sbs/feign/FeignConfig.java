94e7a6702688d33ac8e0d504c7c3b780d8c07b20
zbz <nnzbz@21cn.com>
2018-08-31 12:54:18 +0800

    * Feign调用传递header

--- feign-spring-boot-starter/src/main/java/rebue/sbs/feign/FeignConfig.java
+++ feign-spring-boot-starter/src/main/java/rebue/sbs/feign/FeignConfig.java
@@ -1,20 +1,49 @@
 package rebue.sbs.feign;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
-import feign.Logger;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 @Configuration
 public class FeignConfig {
+    private final static Logger _log = LoggerFactory.getLogger(FeignConfig.class);
 
     /**
      * 只有在这里才能控制feign的日志级别
      */
     @Bean
-    Logger.Level feignLoggerLevel() {
+    feign.Logger.Level feignLoggerLevel() {
 //        return Logger.Level.FULL;
-        return Logger.Level.BASIC;
+        return feign.Logger.Level.BASIC;
     }
 
+    @Bean
+    public RequestInterceptor headerInterceptor() {
+        _log.info("传递接收到的请求头");
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate requestTemplate) {
+                ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+                HttpServletRequest request = attributes.getRequest();
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        String name = headerNames.nextElement();
+                        String values = request.getHeader(name);
+                        requestTemplate.header(name, values);
+                    }
+                }
+            }
+        };
+    }
 }
