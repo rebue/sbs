89a04843737a219446d8792f95306f8790490579
zbz <nnzbz@21cn.com>
2018-09-17 11:42:09 +0800

    jackson支持jdk8的LocalDate等类

--- smx-spring-boot-starter/src/main/java/rebue/sbs/smx/JacksonConfig.java
+++ smx-spring-boot-starter/src/main/java/rebue/sbs/smx/JacksonConfig.java
@@ -2,6 +2,7 @@ package rebue.sbs.smx;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 import org.springframework.boot.json.JsonParser;
 import org.springframework.boot.json.JsonParserFactory;
@@ -9,13 +10,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 /**
  * 初始化Jackson的转换器
@@ -36,7 +40,8 @@ public class JacksonConfig implements WebMvcConfigurer {
 
     @Bean
     public ObjectMapper getObjectMapper() {
-        return new ObjectMapper();
+        return Jackson2ObjectMapperBuilder.json().serializationInclusion(JsonInclude.Include.NON_NULL).featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .timeZone(TimeZone.getTimeZone("Asia/Shanghai")).modules(new JavaTimeModule()).build();
     }
 
     @Override
