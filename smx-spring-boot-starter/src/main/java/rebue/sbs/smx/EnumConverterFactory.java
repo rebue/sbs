9368bac8fdd623dd96960584faff44402d0f6148
zbz <nnzbz@qq.com>
2018-12-28 17:16:47 +0800

    * 补充日志

--- smx-spring-boot-starter/src/main/java/rebue/sbs/smx/EnumConverterFactory.java
+++ smx-spring-boot-starter/src/main/java/rebue/sbs/smx/EnumConverterFactory.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 
+import rebue.wheel.StrUtils;
 import rebue.wheel.baseintf.EnumBase;
 
 /**
@@ -21,8 +22,7 @@ import rebue.wheel.baseintf.EnumBase;
  * </pre>
  */
 public class EnumConverterFactory implements ConverterFactory<String, EnumBase> {
-    private final static Logger                              _log         = LoggerFactory
-            .getLogger(EnumConverterFactory.class);
+    private final static Logger                              _log         = LoggerFactory.getLogger(EnumConverterFactory.class);
 
     private final static Map<Class<?>, Converter<String, ?>> converterMap = new WeakHashMap<>();
 
@@ -33,12 +33,12 @@ public class EnumConverterFactory implements ConverterFactory<String, EnumBase> 
      *            实现了BaseEnum接口的类
      */
     @Override
-    public <T extends EnumBase> Converter<String, T> getConverter(Class<T> targetType) {
+    public <T extends EnumBase> Converter<String, T> getConverter(final Class<T> targetType) {
         _log.info("获取IntegerStr转枚举类型的转换器：targetType={}", targetType.getName());
         @SuppressWarnings("unchecked")
         Converter<String, T> result = (Converter<String, T>) converterMap.get(targetType);
         if (result == null) {
-            result = new IntegerStrToEnumConverter<T>(targetType);
+            result = new IntegerStrToEnumConverter<>(targetType);
             converterMap.put(targetType, result);
         }
         return result;
@@ -50,29 +50,34 @@ public class EnumConverterFactory implements ConverterFactory<String, EnumBase> 
      * @param <T>
      */
     private class IntegerStrToEnumConverter<T extends EnumBase> implements Converter<String, T> {
-        private Map<String, T> enumMap = new HashMap<>();
+        private final Map<String, T> enumMap = new HashMap<>();
 
         /**
          * 构造方法 - 将所有的枚举项加入map中，以便转换的时候查找
          * 
          * @param enumType
          */
-        public IntegerStrToEnumConverter(Class<T> enumType) {
+        public IntegerStrToEnumConverter(final Class<T> enumType) {
             _log.info("将枚举类型的所有项放入map中：enumType={}", enumType.getName());
-            T[] enums = enumType.getEnumConstants();
-            for (T e : enums) {
-                _log.info("{}:{}", e.getCode(), e.getClass().getName());
-                enumMap.put(e.getCode() + "", e);
+            final T[] enums = enumType.getEnumConstants();
+            for (final T enu : enums) {
+                _log.info("{}:{}", enu.getCode(), enu.getClass().getName());
+                enumMap.put(enu.getCode() + "", enu);
             }
         }
 
         @Override
-        public T convert(String source) {
+        public T convert(final String source) {
             _log.info("将{}转成枚举类型", source);
-            T result = enumMap.get(source);
+            final T result = enumMap.get(source);
             if (result == null) {
-                String msg = "枚举类型中没有对应" + source + "的项";
-                _log.error(msg);
+                final String msg = "枚举类型中没有对应" + source + "的项";
+                String sTemp = "";
+                for (final String key : enumMap.keySet()) {
+                    sTemp += key + ",";
+                }
+                sTemp = StrUtils.delRight(sTemp, 1);
+                _log.error(msg + ": 枚举类型有{}", sTemp);
                 throw new IllegalArgumentException(msg);
             }
             _log.info("{}转成枚举类型{}的{}", source, result.getClass().getSimpleName(), ((Enum<?>) result).name());
