0da1159894359052a3aa531fae34c78b343c8340
zbz <nnzbz@21cn.com>
2020-06-14 21:03:18 +0800

    + 统一异常处理

---
+++ aop-spring-boot-starter/src/main/java/rebue/sbs/aop/ApiErrAopConfig.java
@@ -0,0 +1,38 @@
+package rebue.sbs.aop;
+
+import javax.validation.ConstraintViolationException;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.yaml.snakeyaml.constructor.DuplicateKeyException;
+
+import rebue.robotech.dic.ResultDic;
+import rebue.robotech.ro.Ro;
+
+/**
+ * API层异常拦截
+ */
+@Aspect
+@Configuration
+@Order
+public class ApiErrAopConfig {
+
+    @Around("execution(public * *..api.*Api.*(..))")
+    public Object around(final ProceedingJoinPoint joinPoint) throws Throwable {
+        try {
+            return joinPoint.proceed();
+        } catch (final DuplicateKeyException e) {
+            return new Ro<>(ResultDic.FAIL, "操作数据库失败，唯一键重复：" + e.getCause().getMessage(), "500", null);
+        } catch (final IllegalArgumentException e) {
+            return new Ro<>(ResultDic.WARN, "参数不能为空");
+        } catch (final ConstraintViolationException e) {
+            return new Ro<>(ResultDic.WARN, e.getMessage().split(":")[1].trim());
+        } catch (final Throwable e) {
+            return new Ro<>(ResultDic.FAIL, "服务器出现未定义的异常，请联系管理员", "500", null);
+        }
+    }
+
+}
