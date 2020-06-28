708bb215305ef90d2824dd87157b8bea4fcfbd24
zbz <nnzbz@qq.com>
2020-06-28 18:37:46 +0800

    * 补充注释 - 取消自定义hibernate-validator.version，使用spring-boot默认 + 引入Validation校验的功能

--- cfg-spring-boot-starter/src/main/java/rebue/sbs/cfg/StringToByteArrayConverter.java
+++ cfg-spring-boot-starter/src/main/java/rebue/sbs/cfg/StringToByteArrayConverter.java
@@ -4,6 +4,15 @@ import java.io.UnsupportedEncodingException;
 
 import org.springframework.core.convert.converter.Converter;
 
+/**
+ * 字符串转byte[]的转换器
+ * 
+ * 用于读取.properties或yaml文件里的配置项，如果是字符串的，而接收类型是byte[]的，则用到此转换器
+ * 此转换器将会按utf-8编码格式转成byte[]
+ * 
+ * @author zbz
+ *
+ */
 public class StringToByteArrayConverter implements Converter<String, byte[]> {
 
     @Override
