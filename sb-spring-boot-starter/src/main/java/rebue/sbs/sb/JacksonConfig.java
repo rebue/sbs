b00a39e1cb3943f98e72b676bd4db52fc1d7f1b1
zbz <nnzbz@qq.com>
2020-08-19 15:33:49 +0800

    * Jackson支持LocalDateTime序列化

--- sb-spring-boot-starter/src/main/java/rebue/sbs/sb/JacksonConfig.java
+++ sb-spring-boot-starter/src/main/java/rebue/sbs/sb/JacksonConfig.java
@@ -13,24 +13,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
 /**
- * 初始化Jackson的转换器
- * 1. 让spring mvc支持json格式，并且统一编码为utf-8
- * 2. 不显示为null的字段
- * 3. Long to String
+ * 初始化Jackson的转换器 1. 让spring mvc支持json格式，并且统一编码为utf-8 2. 不显示为null的字段 3. Long to String
  *
  * @author zbz
  */
 @Configuration
 public class JacksonConfig implements WebMvcConfigurer {
     /**
-     * Parser that can read JSON formatted strings into Maps or Lists.
-     * 可以用来读取JSON字符串并解析到Map或List
+     * Parser that can read JSON formatted strings into Maps or Lists. 可以用来读取JSON字符串并解析到Map或List
      */
     @Bean
     public JsonParser getJsonParser() {
@@ -52,7 +49,7 @@ public class JacksonConfig implements WebMvcConfigurer {
      */
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return jackson2ObjectMapperBuilder -> setJackson2ObjectMapperBuilder(jackson2ObjectMapperBuilder);
+        return this::setJackson2ObjectMapperBuilder;
     }
 
     private Jackson2ObjectMapperBuilder setJackson2ObjectMapperBuilder(final Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder) {
@@ -62,14 +59,14 @@ public class JacksonConfig implements WebMvcConfigurer {
                 .featuresToEnable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
 //              // 序列化BigDecimal时不使用科学计数法输出
 //              .featuresToEnable(JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN)
-//                .featuresToDisable(
-//                        // 按默认的时间格式'yyyy-MM-dd'T'HH:mm:ss.SSS’转换有时会报错
-//                        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
+                .featuresToDisable(
+                        // 序列化时不按默认的时间格式'yyyy-MM-dd'T'HH:mm:ss.SSS’转换(按JavaTimeModule设置的格式)
+                        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS
 //                        // 对于空的对象转json的时候不抛出错误
 //                        SerializationFeature.FAIL_ON_EMPTY_BEANS,
 //                        // 禁用遇到未知属性抛出异常
 //                        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
-//                )
+                )
                 // 不转换值为null的项
                 .serializationInclusion(JsonInclude.Include.NON_NULL)
                 // 全局转化long类型为String，避免js用number接收long类型时丢失精度问题
