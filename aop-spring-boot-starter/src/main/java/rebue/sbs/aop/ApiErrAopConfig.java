2d4711a49717e20093f87cf3d1e19635449ed752
zbz <nnzbz@21cn.com>
2021-01-07 18:26:12 +0800

    * 完善异常捕获的不同情况

--- aop-spring-boot-starter/src/main/java/rebue/sbs/aop/ApiErrAopConfig.java
+++ aop-spring-boot-starter/src/main/java/rebue/sbs/aop/ApiErrAopConfig.java
@@ -1,5 +1,7 @@
 package rebue.sbs.aop;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import javax.validation.ConstraintViolationException;
 
 import org.apache.commons.lang3.StringUtils;
@@ -8,11 +10,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 
 import lombok.extern.slf4j.Slf4j;
 import rebue.robotech.dic.ResultDic;
 import rebue.robotech.ro.Ro;
+import rebue.wheel.exception.RuntimeExceptionX;
 
 /**
  * API层异常拦截
@@ -35,7 +39,7 @@ public class ApiErrAopConfig {
             return new Ro<>(ResultDic.WARN, message.substring(start, end) + "已存在");
         } catch (final IllegalArgumentException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return new Ro<>(ResultDic.PARAM_ERROR, "参数错误: ");
+                return new Ro<>(ResultDic.PARAM_ERROR, "参数错误");
             }
             else {
                 return new Ro<>(ResultDic.PARAM_ERROR, "参数错误: " + e.getMessage());
@@ -47,23 +51,31 @@ public class ApiErrAopConfig {
                 sb.append(err.split(":")[1].trim() + ",");
             }
             return new Ro<>(ResultDic.PARAM_ERROR, sb.deleteCharAt(sb.length() - 1).toString());
+        } catch (final DataIntegrityViolationException e) {
+            return returnSQL((SQLIntegrityConstraintViolationException) e.getCause());
         } catch (final NullPointerException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return new Ro<>(ResultDic.FAIL, "空指针异常", null, "500", null);
+                return new Ro<>(ResultDic.FAIL, "服务器出现空指针异常", null, "500", null);
             }
             else {
-                return new Ro<>(ResultDic.FAIL, e.getMessage(), null, "500", null);
+                return new Ro<>(ResultDic.FAIL, "服务器出现空指针异常", e.getMessage(), "500", null);
             }
+        } catch (final RuntimeExceptionX e) {
+            return new Ro<>(ResultDic.WARN, e.getMessage());
         } catch (final RuntimeException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return new Ro<>(ResultDic.FAIL, "运行时异常", null, "500", null);
+                return new Ro<>(ResultDic.FAIL, "服务器出现运行时异常", null, "500", null);
             }
             else {
-                return new Ro<>(ResultDic.FAIL, e.getMessage(), null, "500", null);
+                return new Ro<>(ResultDic.FAIL, "服务器出现运行时异常", e.getMessage(), "500", null);
             }
         } catch (final Throwable e) {
             return new Ro<>(ResultDic.FAIL, "服务器出现未定义的异常，请联系管理员", e.getMessage(), "500", null);
         }
+    }
+
+    private Ro<String> returnSQL(final SQLIntegrityConstraintViolationException e) {
+        return new Ro<>(ResultDic.WARN, "此数据被其它数据关联引用，不能直接修改或删除", e.getMessage());
     }
 
 }
--- aop-spring-boot-starter/src/main/java/rebue/sbs/aop/CtrlErrAopConfig.java
+++ aop-spring-boot-starter/src/main/java/rebue/sbs/aop/CtrlErrAopConfig.java
@@ -10,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 import rebue.robotech.dic.ResultDic;
 import rebue.robotech.ro.Ro;
+import rebue.wheel.exception.RuntimeExceptionX;
 
 /**
  * API层异常拦截
@@ -25,28 +26,34 @@ public class CtrlErrAopConfig {
             return joinPoint.proceed();
         } catch (final IllegalArgumentException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.PARAM_ERROR, "参数错误: ")));
+                return returnRoInCatch(new Ro<>(ResultDic.PARAM_ERROR, "参数错误 "));
             }
             else {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.PARAM_ERROR, "参数错误: " + e.getMessage())));
+                return returnRoInCatch(new Ro<>(ResultDic.PARAM_ERROR, "参数错误: " + e.getMessage()));
             }
         } catch (final NullPointerException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.FAIL, "空指针异常", null, "500", null)));
+                return returnRoInCatch(new Ro<>(ResultDic.FAIL, "服务器出现空指针异常", null, "500", null));
             }
             else {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.FAIL, e.getMessage(), null, "500", null)));
+                return returnRoInCatch(new Ro<>(ResultDic.FAIL, "服务器出现空指针异常", e.getMessage(), "500", null));
             }
+        } catch (final RuntimeExceptionX e) {
+            return returnRoInCatch(new Ro<>(ResultDic.WARN, e.getMessage()));
         } catch (final RuntimeException e) {
             if (StringUtils.isBlank(e.getMessage())) {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.FAIL, "运行时异常", null, "500", null)));
+                return returnRoInCatch(new Ro<>(ResultDic.FAIL, "服务器出现运行时异常", null, "500", null));
             }
             else {
-                return Mono.create(callback -> callback.success(new Ro<>(ResultDic.FAIL, e.getMessage(), null, "500", null)));
+                return returnRoInCatch(new Ro<>(ResultDic.FAIL, "服务器出现运行时异常", e.getMessage(), "500", null));
             }
         } catch (final Throwable e) {
-            return Mono.create(callback -> callback.success(new Ro<>(ResultDic.FAIL, "服务器出现未定义的异常，请联系管理员", e.getMessage(), "500", null)));
+            return returnRoInCatch(new Ro<>(ResultDic.FAIL, "服务器出现未定义的异常，请联系管理员", e.getMessage(), "500", null));
         }
+    }
+
+    private Mono<?> returnRoInCatch(final Ro<?> ro) {
+        return Mono.create(callback -> callback.success(ro));
     }
 
 }
