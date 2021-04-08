0e8a0315c0b006ea0f83e247ed7a94b94a906d6a
zbz <nnzbz@qq.com>
2021-04-08 18:37:44 +0800

    * 配置redis缓存使用jackson序列化

--- cache-spring-boot-starter/src/main/java/rebue/sbs/cache/CacheConfig.java
+++ cache-spring-boot-starter/src/main/java/rebue/sbs/cache/CacheConfig.java
@@ -4,8 +4,11 @@ import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 /**
  * 多缓存配置
@@ -17,8 +20,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 public class CacheConfig {
     @Bean(CacheManagerName.REDIS_CACHE_MANAGER)
     @Primary
-    public RedisCacheManager redisCacheManager(final RedisTemplate<String, String> redisTemplate) {
-        return RedisCacheManager.builder().build();
+    public RedisCacheManager cacheManager(final RedisConnectionFactory connectionFactory) {
+        // return RedisCacheManager.create(connectionFactory);
+        return RedisCacheManager.builder(connectionFactory)
+            .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())))
+            .build();
     }
 
     @Bean(CacheManagerName.CAFFEINE_CACHE_MANAGER)
