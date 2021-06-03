adf865dc4f8702785993bbefec5c08e3d5428b4c
zbz <nnzbz@qq.com>
2021-06-03 13:05:55 +0800

    * webflux保存请求对象改为保存响应对象

--- sb-spring-boot-starter/src/main/java/rebue/sbs/sb/ctx/ReactiveRequestContextFilter.java
+++ sb-spring-boot-starter/src/main/java/rebue/sbs/sb/ctx/ReactiveRequestContextFilter.java
@@ -3,7 +3,7 @@ package rebue.sbs.sb.ctx;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
@@ -16,8 +16,8 @@ public class ReactiveRequestContextFilter implements WebFilter, Ordered {
 
     @Override
     public Mono<Void> filter(final ServerWebExchange exchange, final WebFilterChain chain) {
-        final ServerHttpRequest request = exchange.getRequest();
-        return chain.filter(exchange).subscriberContext(ctx -> ctx.put(ReactiveRequestContextHolder.CONTEXT_KEY, request));
+        final ServerHttpResponse response = exchange.getResponse();
+        return chain.filter(exchange).subscriberContext(ctx -> ctx.put(ReactiveResponseContextHolder.CONTEXT_KEY, response));
     }
 
     @Override
--- sb-spring-boot-starter/src/main/java/rebue/sbs/sb/ctx/ReactiveResponseContextHolder.java
+++ sb-spring-boot-starter/src/main/java/rebue/sbs/sb/ctx/ReactiveResponseContextHolder.java
@@ -1,19 +1,19 @@
 package rebue.sbs.sb.ctx;
 
-import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 
 import reactor.core.publisher.Mono;
 import reactor.util.context.Context;
 
-public class ReactiveRequestContextHolder {
-    static final Class<ServerHttpRequest> CONTEXT_KEY = ServerHttpRequest.class;
+public class ReactiveResponseContextHolder {
+    static final Class<ServerHttpResponse> CONTEXT_KEY = ServerHttpResponse.class;
 
     /**
      * Gets the {@code Mono<ServerHttpRequest>} from Reactor {@link Context}
      *
      * @return the {@code Mono<ServerHttpRequest>}
      */
-    public static Mono<ServerHttpRequest> getRequest() {
+    public static Mono<ServerHttpResponse> getResponse() {
         return Mono.subscriberContext().map(ctx -> ctx.get(CONTEXT_KEY));
     }
 
