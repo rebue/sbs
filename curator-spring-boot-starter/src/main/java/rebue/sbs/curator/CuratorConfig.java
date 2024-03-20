6faf1cd28c030245582783b05273928b501d2182
zbz <nnzbz@21cn.com>
2024-03-20 15:03:12 +0800

    * CuratorConfig默认开启

--- curator-spring-boot-starter/src/main/java/rebue/sbs/curator/CuratorConfig.java
+++ curator-spring-boot-starter/src/main/java/rebue/sbs/curator/CuratorConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 // XXX 启用属性类(也就是注入属性类，如果没有这一行，属性类要另外写注入，如在属性类上加注解@Compenent，或扫描)
 @EnableConfigurationProperties(CuratorProperties.class)
-@ConditionalOnExpression("${curator.enabled:false}")
+@ConditionalOnExpression("${curator.enabled:true}")
 public class CuratorConfig {
     @Bean
     public RetryPolicy retryPolicy(final CuratorProperties properties) {
