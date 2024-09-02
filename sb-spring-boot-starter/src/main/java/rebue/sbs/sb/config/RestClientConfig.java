6f153ae4c9056db2eab023c5a9276030257cf9a3
zbz <nnzbz@qq.com>
2024-09-02 16:08:35 +0800

    + 注入RestTemplate和RestClient

---
+++ sb-spring-boot-starter/src/main/java/rebue/sbs/sb/config/RestClientConfig.java
@@ -0,0 +1,39 @@
+package rebue.sbs.sb.config;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * RestClient配置
+ */
+@Configuration(proxyBeanMethods = false)
+public class RestClientConfig {
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(5000);// 单位为ms
+        factory.setConnectTimeout(5000);// 单位为ms
+        return factory;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
+        RestTemplate restTemplate = new RestTemplate(factory);
+        // 支持中文编码
+        restTemplate.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        return restTemplate;
+
+    }
+
+    @Bean
+    public RestClient restClient(RestTemplate restTemplate) {
+        return RestClient.create(restTemplate);
+    }
+}
